1027eeb49fee30013218492e407b41e633e1335d
Alexey Khalaydzhi <aleksei_halaidzh@mail.ru>
2021-10-16 15:28:44 +0300

    HW-3

---
+++ linters/run.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+
+function print_header() {
+    echo -e "\n***** ${1} *****"
+}
+
+print_header "RUN cppcheck"
+cppcheck project --enable=all --inconclusive --error-exitcode=1 -I project/include -I project/tests/include --suppress=missingIncludeSystem
+
+print_header "RUN clang-tidy"
+clang-tidy project/src/* project/include/* -warnings-as-errors=* -extra-arg=-std=c99 -- -Iproject/include
+
+print_header "RUN cpplint.py"
+python3 ./linters/cpplint/cpplint.py --extensions=c project/include/* project/src/*
+
+print_header "SUCCESS"

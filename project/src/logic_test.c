47a733f4ff70b29e6e752c41381755a09a4bdeeb
Ilya <mr.ilya868@mail.ru>
2021-10-24 22:23:46 +0300

    Removed copy-paste

--- project/src/logic_test.c
+++ project/src/logic_test.c
@@ -1,40 +1,35 @@
 #include "input_output_test.c"
 #include "logic_test.h"
 
+#define OUTPUT_MESSAGE \
+	"1 Number account: ", \
+	"2 Client name: ", \
+	"3 Surname: ", \
+	"4 Addres client: ", \
+	"5 Client Telnum: ", \
+	"6 Client indebtedness: ", \
+	"7 Client credit limit: ", \
+	"8 Client cash payments: "
+#define SCAN_STRUCTURE \
+	&client.number, \
+	client.name, \
+	client.surname, \
+	client.addres, \
+	client.tel_number, \
+	&client.indebtedness, \
+	&client.credit_limit, \
+	&client.cash_payments
+
 void write_data_to_file() {
 	Data client = {0};
 	FILE *Ptr = fopen("record.dat", "r+");
 	if(Ptr == NULL) {
 		puts("Not acess");
 	} else {
-		printf("%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n\n",
-			"1 Number account: ",
-			"2 Client name: ",
-			"3 Surname: ",
-			"4 Addres client: ",
-			"5 Client Telnum: ",
-			"6 Client indebtedness: ",
-			"7 Client credit limit: ",
-			"8 Client cash payments: ");
-		while(scanf("%12d%11s%11s%16s%20s%12lf%12lf%12lf",
-				&client.number,
-				client.name,
-				client.surname,
-				client.addres,
-				client.tel_number,
-				&client.indebtedness,
-				&client.credit_limit,
-				&client.cash_payments) != -1) {
+		printf("%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n\n", OUTPUT_MESSAGE);
+		while(scanf("%12d%11s%11s%16s%20s%12lf%12lf%12lf", SCAN_STRUCTURE) != -1) {
 			write_to_file(Ptr, &client);
-			printf("%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n\n",
-				"1 Number account: ",
-				"2 Client name: ",
-				"3 Surname: ",
-				"4 Addres client: ",
-				"5 Client Telnum: ",
-				"6 Client indebtedness: ",
-				"7 Client credit limit: ",
-				"9 Client cash payments:");
+			printf("%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n\n", OUTPUT_MESSAGE);
 		}
 		fclose(Ptr);
 	}
@@ -60,29 +55,31 @@ void transaction_write() {
 }
 
 void write_transfer_to_file() {
-	Data client_data = {0}, transfer = {0};
+	Data client = {0}, transfer = {0};
 	FILE *Ptr, *Ptr_2, *blackrecord;
 	Ptr = fopen("record.dat", "r");
 	Ptr_2 = fopen("transaction.dat", "r");
 	blackrecord = fopen("blackrecord.dat", "w");
-	if (Ptr == NULL || Ptr_2 == NULL || blackrecord == NULL) {
+	if (Ptr == NULL) {
 		puts("exit");
+		fclose(Ptr_2);
+		fclose(blackrecord);
+	} else if (Ptr_2 == NULL) {
+		puts("exit");
+		fclose(Ptr);
+		fclose(blackrecord);
+	} else if (blackrecord == NULL) {
+		puts("exit");
+		fclose(Ptr);
+		fclose(Ptr_2);
 	} else {
-		while (fscanf(Ptr, "%12d%11s%11s%16s%20s%12lf%12lf%12lf",
-			&client_data.number,
-			client_data.name,
-			client_data.surname,
-			client_data.addres,
-			client_data.tel_number,
-			&client_data.indebtedness,
-			&client_data.credit_limit,
-			&client_data.cash_payments) != -1) {
+		while (fscanf(Ptr, "%12d%11s%11s%16s%20s%12lf%12lf%12lf", SCAN_STRUCTURE) != -1) {
 			while (fscanf(Ptr_2, "%3d%6lf", &transfer.number, &transfer.cash_payments) != -1) {
-				if (client_data.number == transfer.number && transfer.cash_payments != 0) {
-					client_data.credit_limit += transfer.cash_payments;
+				if (client.number == transfer.number && transfer.cash_payments != 0) {
+					client.credit_limit += transfer.cash_payments;
 				}
 			}
-			write_to_file(blackrecord, &client_data);
+			write_to_file(blackrecord, &client);
 			rewind(Ptr_2);
 		}
 		fclose(Ptr);
